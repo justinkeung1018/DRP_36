d86426f4a06112818af816245713674b76ff9f03
Aboud Dabbas <ad2122@ic.ac.uk>
2024-05-28 22:44:11 +0100

    [@ad2122] Added firebase configuration

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
+import database from "./firebase";
+import {ref, get} from "firebase/database";
 
 export const App = () => {
+  // const data = ref(database, "hi");
+  // get(data).then((snapshot) => {
+  //   if (snapshot.exists()) {
+  //     console.log(snapshot.val());
+  //   } else {
+  //     console.log("No data available");
+  //   }
+  // }).catch((error) => {
+  //   console.error(error);
+  // });
   return (
     <div className="container mx-auto py-10">
       DRP
---
+++ src/client/firebase.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import {get, ref, getDatabase} from "firebase/database";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApBcH7XHno5saByD1_IvbGqnC1K8cEDqw",
+  authDomain: "drp36-ea396.firebaseapp.com",
+  projectId: "drp36-ea396",
+  storageBucket: "drp36-ea396.appspot.com",
+  messagingSenderId: "1064933229081",
+  appId: "1:1064933229081:web:b1ac20faf42545ff186088",
+  measurementId: "G-99RTYS3R8C",
+  databaseURL: "https://drp36-ea396-default-rtdb.europe-west1.firebasedatabase.app",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+export default database;
