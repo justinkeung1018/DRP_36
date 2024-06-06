fb0cfad051e0c45c8a50735ae0de7edd57ac932c
Aboud Dabbas <ad2122@ic.ac.uk>
2024-06-06 18:16:53 +0100

    [@ad2122] Updated firebase config

--- src/client/firebase.tsx
+++ src/client/firebase.tsx
@@ -11,15 +11,26 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAakMxuEU9lC6a8m67ztllTRxUyYswo7z8",
-  authDomain: "drp36-e0704.firebaseapp.com",
-  projectId: "drp36-e0704",
-  storageBucket: "drp36-e0704.appspot.com",
-  messagingSenderId: "861245857051",
-  appId: "1:861245857051:web:7e02a77ddb09ad136b47be",
-  databaseURL:
-    "https://drp36-e0704-default-rtdb.europe-west1.firebasedatabase.app",
+  apiKey: "AIzaSyDKZc2L9MFlyKnT7zQqzwaE5VUf4DItRjo",
+  authDomain: "icfood-4334b.firebaseapp.com",
+  projectId: "icfood-4334b",
+  storageBucket: "icfood-4334b.appspot.com",
+  messagingSenderId: "412745917777",
+  appId: "1:412745917777:web:55b551adb6422f0f555f70",
+  databaseURL: "https://icfood-4334b-default-rtdb.europe-west1.firebasedatabase.app/"
 };
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAakMxuEU9lC6a8m67ztllTRxUyYswo7z8",
+//   authDomain: "drp36-e0704.firebaseapp.com",
+//   projectId: "drp36-e0704",
+//   storageBucket: "drp36-e0704.appspot.com",
+//   messagingSenderId: "861245857051",
+//   appId: "1:861245857051:web:7e02a77ddb09ad136b47be",
+//   databaseURL:
+//     "https://drp36-e0704-default-rtdb.europe-west1.firebasedatabase.app",
+// };
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
